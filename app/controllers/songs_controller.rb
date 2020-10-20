22398cf27cadadad3e1d15d83244da7159cc1ff8
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-20 23:37:28 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,8 +20,6 @@ class SongsController < ApplicationController
 
   def show
     @song = Song.find(params[:id])
-    @genre = Genre.find(@song.genre_id)
-    @artist = Artist.find(@song.artist_id)
   end
 
   def update
